fa5bc646dc49df38339ac96ad48e9690fddb9ab7
FelixHertweck <github@felix-hertweck.de>
2025-12-12 10:22:48 +0100

    Initial setup of next.js app and workflow setup

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import process from "process";
+
+const nextConfig = () => {
+  const buildMode = process.env.BUILD_MODE;
+
+  if (buildMode === "static") {
+    return {
+      output: "export",
+      basePath: process.env.PAGES_BASE_PATH,
+      assetPrefix: process.env.PAGES_BASE_PATH + "/",
+    };
+  }
+
+  if (buildMode === "standalone") {
+    return {
+      output: "standalone",
+    };
+  }
+  return {};
+};
+
+export default nextConfig;
